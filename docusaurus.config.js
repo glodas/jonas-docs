c8e7d6ac3b1669afb14c3e2bbed828562e400049
Jonas Andrijauskas <glodas@gmail.com>
2023-05-31 23:39:13 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Jonas Blog',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Jonas Blog. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
