5b55efdfcdfeaca8a442f0f17d2dadc3cd351a10
Jonas Andrijauskas <glodas@gmail.com>
2023-06-05 22:26:42 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '21.lt',
+  tagline: 'Kasdieniškos mintys ir atradimai',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://21.lt',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+ // organizationName: 'facebook', // Usually your GitHub org/user name.
+  //projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -77,26 +77,26 @@ const config = {
             label: 'Tutorial',
           },*/
           {to: '/blog', label: 'Blog', position: 'left'},
-          {
+          {/*
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
-            position: 'right',
+            position: 'right',*/
           },
         ],
       },
       footer: {
         style: 'dark',
         links: [
-          {
+          {/*
             title: 'Docs',
             items: [
               {
                 label: 'Tutorial',
                 to: '/docs/intro',
               },
-            ],
+            ],*/
           },
-          {
+          {/*
             title: 'Community',
             items: [
               {
@@ -112,7 +112,7 @@ const config = {
                 href: 'https://twitter.com/docusaurus',
               },
             ],
-          },
+        */  },
           {
             title: 'More',
             items: [
@@ -120,9 +120,9 @@ const config = {
                 label: 'Blog',
                 to: '/blog',
               },
-              {
+              {/*
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/facebook/docusaurus',*/
               },
             ],
           },
