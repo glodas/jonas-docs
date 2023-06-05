40cdb39fad25d593c4db4b743885c71c64aec9b1
Jonas Andrijauskas <glodas@gmail.com>
2023-06-05 22:51:23 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,9 +78,9 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+           // href: 'https://github.com/facebook/docusaurus',
+           // label: 'GitHub',
+           // position: 'right',
           },
         ],
       },
